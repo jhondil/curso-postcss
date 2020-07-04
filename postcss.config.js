31923ac9907cad25006007c6549e3fe029e54585
Jhonatan Ibarra <ibarra.jhonatann@gmail.com>
2020-07-03 21:22:31 -0500

    segundo modulo de Postcss

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,13 @@ module.exports = {
      require('postcss-preset-env')({
      autoprefixer:{
        grid:true,
+       stage:1,
      }
-    })
+    }),
+    require('postcss-apply'),
+    require('postcss-custom-media')({
+      preserve: false,
+  }),
+  require('postcss-custom-selectors')
   ]
 }
