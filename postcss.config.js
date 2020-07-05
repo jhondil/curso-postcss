de38bb3b5327fe0df02d400bd408fc57996d8fb3
Jhonatan Ibarra <ibarra.jhonatann@gmail.com>
2020-07-04 20:01:25 -0500

    fin del curso :D

--- postcss.config.js
+++ postcss.config.js
@@ -6,13 +6,24 @@ module.exports = {
        stage:1,
      }
     }),
+    require('stylelint'),
     require('postcss-custom-selectors'),
     require('postcss-apply'),
     require('postcss-import'),    
     require('postcss-nesting'),
+    require('stylelint'),
+    require('postcss-font-magician')({
+      variants: {
+        'Lato':{
+          '300': ['woff'],
+          '400': []
+        }
+      }
+    }),
     require('postcss-custom-media')({
       preserve: false,
-  })
+  }),
+  require('cssnano'),
  
   ]
 }
